6adcfd1ca6e7ff6c2d0c8ec82b095bee857dbcd4
Ivan Utkin <iutkin@ethz.ch>
2022-02-16 04:36:44 +0100

    WIP

--- visualization/visme.m
+++ visualization/visme.m
@@ -1,6 +1,10 @@
 clear;figure(1);clf;colormap turbo
 simdir = '../results/out2';
-load([simdir '/params.mat']);whos
+% load([simdir '/params.mat']);whos
+rc2,pc2 = ndgrid()
 for it = 1:10
-    
+    load(sprintf('%s/step_%d.mat',simdir,it))
+    tiledlayout(2,2,'TileSpacing','compact','Padding','compact')
+    nexttile
+
 end
