46e43142fb1afce74a6c61674430b766290a84b0
Ivan Utkin <iutkin@ethz.ch>
2022-02-19 23:13:16 +0100

    WIP

--- visualization/fig_spinodal_nucleation.m
+++ visualization/fig_spinodal_nucleation.m
@@ -6,15 +6,38 @@ set(gcf,'Color','white','Units','centimeters','Position',[15 3 12 6])
 mode = 'no';
 
 simdirs = {['../results/out_spinodal_' mode '_shear_no_coupling']  ...
-    ,      ['../results/out_nucleation_' mode '_shear_no_coupling']};
+    ,      ['../results/out_nucleation_' mode '_shear_no_coupling'] ...
+    ,      ['../results/out_inclusion_' mode '_shear']};
 
 
-its = [1000,1000];
+its = [1000,1000,100];
 
 itile = 0;
 cb_oy = 0.18;
 
-tiledlayout(1,2,'TileSpacing','tight','Padding','compact')
+tiledlayout(2,3,'TileSpacing','tight','Padding','compact')
+
+for isim = 1:numel(simdirs)
+    simdir = simdirs{isim};
+    load([simdir '/params.mat'])
+    [rc2,pc2] = ndgrid(rc,pc);
+    xc        = rc2.*cos(pc2);
+    yc        = rc2.*sin(pc2);
+    it        = 1;
+
+    load(sprintf('%s/step_%d.mat',simdir,it))
+
+    nexttile
+    pcolor(xc,yc,C);shading flat;axis image
+
+    caxis([0 1])
+    axis off;colormap(gca,flip(gray))
+    set(gca,'FontSize',fs)
+%     xlim([0 r0+lr]); ylim([0 r0+lr])
+    text(0.0,1.0,['\bf' char('A'+itile)],'units','normalized','FontSize',fs+1)
+    
+    itile = itile + 1;
+end
 
 for isim = 1:numel(simdirs)
     simdir = simdirs{isim};
@@ -34,6 +57,7 @@ for isim = 1:numel(simdirs)
     set(gca,'FontSize',fs)
 %     xlim([0 r0+lr]); ylim([0 r0+lr])
     text(0.0,1.0,['\bf' char('A'+itile)],'units','normalized','FontSize',fs+1)
+    
     itile = itile + 1;
 end
 
